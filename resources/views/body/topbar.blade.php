50f14893f759042728f462f5cc31efea4324a270
king <kelvinozak0@gmail.com>
2025-09-28 21:11:33 +0100

    Homepage Styling

---
+++ resources/views/body/topbar.blade.php
@@ -0,0 +1,26 @@
+<nav id="main-nav">
+    <a href="#" class="js-nav-toggle nav-toggle active"><i></i></a>
+    <div class="js-fullheight table">
+    <div class="table-cell js-fullheight">
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="resume.html">Resume</a></li>
+            <li><a href="work.html">Portfolio</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+        <!-- Social -->
+        <p class="social-icon">
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+        </p>
+    </div>
+    </div>
+</nav>
+<!-- Scroll icon -->
+<div class="scroll-icon">
+    <a href="#info" class="smoothscroll">
+    <div class="mouse"></div>
+    </a>
+    <div class="end-top"></div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,94 +1,71 @@
 <!DOCTYPE HTML>
 <html lang="en">
-
-<!-- index.html  26 Nov 2019 03:50:59 GMT -->
 <head>
-      <!--Meta Tags-->
-      <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="" />
-      <meta name="keywords" content="" />
-      <meta name="author" content="" />
-      <!--Favicons-->
-      <link rel="shortcut icon" href="images/favicon.png">
-      <!--Page Title-->
-      <title>King - Personal Portfolio</title>
-      <!-- Bootstrap  -->
-      <link rel="stylesheet" href="css/bootstrap.css">
-      <!-- Animate.css -->
-      <link rel="stylesheet" href="css/animate.css">
-      <!-- Awesome Fonts-->
-      <link rel="stylesheet" href="css/all.min.css">
-      <!-- Flexslider  -->
-      <link rel="stylesheet" href="css/flexslider.css">
-      <!--Main Styles Css-->
-      <link rel="stylesheet" href="css/style.css">
-   </head>
-   <body>
+    <!--Meta Tags-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <!--Favicons-->
+    <link rel="shortcut icon" href="{{asset('images/favicon.png')}}">
+    <!--Page Title-->
+    <title>King - Personal Portfolio</title>
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="{{asset('css/animate.css')}}">
+    <!-- Awesome Fonts-->
+    <link rel="stylesheet" href="{{asset('css/all.min.css')}}">
+    <!-- Flexslider  -->
+    <link rel="stylesheet" href="{{asset('css/flexslider.css')}}">
+    <!--Main Styles Css-->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="pre-container">
+        <div class="spinner">
+            <div class="double-bounce1"></div>
+            <div class="double-bounce2"></div>
+        </div>
+        </div>
+    </div>
+    <!-- Εnd Preloader -->
+    <div id="page">
+    <!-- header -->
+    @include('body.header')
+    <!-- End Header -->
 
-      <!-- Preloader -->
-      <div id="preloader">
-         <div class="pre-container">
-            <div class="spinner">
-               <div class="double-bounce1"></div>
-               <div class="double-bounce2"></div>
-            </div>
-         </div>
-      </div>
-      <!-- Εnd Preloader -->
+    <!-- Main content section -->
+    @yield('sections')
 
-    
+    <!-- Footer -->
+    @include('body.footer')
+    <!-- End Footer -->
+    </div>
 
-         <!-- header -->
-
-         <!-- End Header -->
-
-         <!-- Banner section -->
-
-         <!-- End Banner Section -->
-
-         <!-- About Section -->
-
-         <!-- End About Section -->
-
-         <!-- Resume Section -->
-
-         <!-- End Resume Section -->
-
-         <!-- Works Section -->
-
-         <!-- End Works Section -->
-
-         <!-- Contact Section -->
-
-         <!-- End Contact Section -->
-
-         <!-- Footer -->
-
-         <!-- End Footer -->
-      </div>
-      <!-- jQuery -->
-      <script src="js/jquery.min.js"></script>
-      <!-- jQuery Easing -->
-      <script src="js/jquery.easing.1.3.js"></script>
-      <!-- Bootstrap -->
-      <script src="js/bootstrap.min.js"></script>
-      <!-- Waypoints -->
-      <script src="js/jquery.waypoints.min.js"></script>
-      <!-- Counters -->
-      <script src="js/jquery.countTo.js"></script>
-      <!-- Flexslider -->
-      <script src="js/jquery.flexslider-min.js"></script>
-      <!-- isotope js -->
-      <script src="js/isotope.pkgd.min.js"></script>
-      <!-- isotope image loader js -->
-      <script src="js/imagesloaded.pkgd.min.js"></script>
-      <!-- Typed -->
-      <script src="js/typed.min.js"></script>
-      <!-- Custom JS -->
-      <script src="js/custom.js"></script>
-   </body>
-
-<!-- index.html  26 Nov 2019 03:52:23 GMT -->
+    <!-- jQuery -->
+    <script src="{{asset('js/jquery.min.js')}}"></script>
+    <!-- jQuery Easing -->
+    <script src="{{asset('js/jquery.easing.1.3.js')}}"></script>
+    <!-- Bootstrap -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- Waypoints -->
+    <script src="{{asset('js/jquery.waypoints.min.js')}}"></script>
+    <!-- Counters -->
+    <script src="{{asset('js/jquery.countTo.js')}}"></script>
+    <!-- Flexslider -->
+    <script src="{{asset('js/jquery.flexslider-min.js')}}"></script>
+    <!-- isotope js -->
+    <script src="{{asset('js/isotope.pkgd.min.js')}}"></script>
+    <!-- isotope image loader js -->
+    <script src="{{asset('js/imagesloaded.pkgd.min.js')}}"></script>
+    <!-- Typed -->
+    <script src="{{asset('js/typed.min.js')}}"></script>
+    <!-- Custom JS -->
+    <script src="{{asset('js/custom.js')}}"></script>
+</body>
 </html>
