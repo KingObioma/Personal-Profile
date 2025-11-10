7b3d80e51befc3f74442f5b51b4e9620ce2e58b0
king <kelvinozak0@gmail.com>
2025-11-10 21:19:26 +0100

    possible light mode

--- resources/views/body/footer.blade.php
+++ resources/views/body/footer.blade.php
@@ -8,7 +8,7 @@
                 <!-- Social -->
                 <p class="social-icon">
                     <a href="https://www.linkedin.com/in/king-obioma-53a266382" target="_blank"><i class="fab fa-linkedin"></i></a>
-                    {{-- <a href="https://www.instagram.com/iamkingobioma" target="_blank"><i class="fab fa-instagram"></i></a> --}}
+                    <a href="https://www.instagram.com/iamkingobioma" target="_blank"><i class="fab fa-instagram"></i></a>
                     <a href="https://www.facebook.com/iamkingobioma" target="_blank"><i class="fab fa-facebook-square"></i></a>
                     <a href="https://wa.me/2349132667481?text=Hello%20King!%20I%20just%20checked%20your%20portfolio%20and%20I’m%20interested%20in%20working%20with%20you." target="_blank"><i class="fab fa-whatsapp-square"></i></a>
                 </p>
