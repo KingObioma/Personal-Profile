670c3aa9c5fbcc74811aa68064f6b72184719ab4
king <kelvinozak0@gmail.com>
2025-12-13 22:19:11 +0100

    new changes

--- resources/views/body/banner.blade.php
+++ resources/views/body/banner.blade.php
@@ -23,6 +23,7 @@
                             <span>Online Conversion Engines</span>
                             <span>conversion-driven designs</span>
                             <span>high-performance sites</span>
+                            <span>high-performance sites</span>
                            </span>
                         </h3>
                      </div>
